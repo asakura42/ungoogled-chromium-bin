05ed8a2625f960cc92bec1d272b7be625ddf8be6
asakura42 <51290160+asakura42@users.noreply.github.com>
2021-01-12 04:33:09 +0000

    fix

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 temp=$(mktemp)
 version=$(curl -sL "https://download.opensuse.org/repositories/home:/justkidding:/arch/standard/x86_64/" | sed 's/<[^>]*>//g'| grep -o "ungoogled-chromium-[[:digit:]].*-x86_64.pkg.tar.zst")
-download=$(curl -sL "https://download.opensuse.org/repositories/home:/justkidding:/arch/standard/x86_64/$version.mirrorlist" | grep -m1 "http.*87.0.4280.88" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u)
+download=$(curl -sL "https://download.opensuse.org/repositories/home:/justkidding:/arch/standard/x86_64/$version.mirrorlist" | grep -m1 "http.*$version" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u)
 curl -L "$download" -o "$temp"
 shasum=$(sha256sum "$temp" | cut -d' ' -f1)
 pkgver="$(egrep -o "([0-9]{1,}\.)+[0-9]{1,}" <<< "$version")"
