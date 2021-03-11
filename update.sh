743ac7d5aa2e52ad1b9a154550e1ce6bff27e0dd
asakura42 <51290160+asakura42@users.noreply.github.com>
2021-03-11 10:54:07 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-temp=$(mktemp)
-version=$(curl -sL "https://download.opensuse.org/repositories/home:/justkidding:/arch/standard/x86_64/" | sed 's/<[^>]*>//g'| grep -o "ungoogled-chromium-[[:digit:]].*-x86_64.pkg.tar.zst" | sort -u | head -n1)
-download=$(curl -sL "https://download.opensuse.org/repositories/home:/justkidding:/arch/standard/x86_64/$version.mirrorlist" | grep -m1 "$version" | sed 's/^.*="/https:\/\/download.opensuse.org/;s/".*$//')
-curl -L "$download" -o "$temp"
-shasum=$(sha256sum "$temp" | cut -d' ' -f1)
-pkgver="$(egrep -o "([0-9]{1,}\.)+[0-9]{1,}" <<< "$version")"
-pkgrel="$(grep -o "\-[[:digit:]]*\-" <<< "$version" | sed 's/-//g')"
-sed -i "s/^pkgver=.*/pkgver=$pkgver/" PKGBUILD
-sed -i "s/^pkgrel=.*/pkgrel=$pkgrel/" PKGBUILD
-sed -i "s/^sha256sums=.*/sha256sums=(\"$shasum\")/" PKGBUILD
-git commit . -m "bump version: $pkgver"
+temp=$(mktemp -d)
+links=$(curl -s "https://repo.vin.ovh/arch/x86_64/" | grep ungoogled | grep -o "ungoogled.*zst" | sort -u | sed 's|^|https://repo.vin.ovh/arch/x86_64/|')
+zst=$(curl -s "https://repo.vin.ovh/arch/x86_64/" | grep ungoogled | grep -o "ungoogled.*zst" | sort -u)
+curl -L -O --output-dir "$temp" "$links.sig"
+curl -L -O --output-dir "$temp" "$links"
+gpg --recv-keys 3DEA62513C8035383A245A12E5786B42E8E5D565
+gpg "$temp/$zst.sig"
+if [ $? -eq 0 ]
+then
+	version=$(ls -1 "$temp" | head -n1)
+	pkgver="$(egrep -o "([0-9]{1,}\.)+[0-9]{1,}" <<< "$version")"
+	pkgrel="$(grep -o "\-[[:digit:]]*\-" <<< "$version" | sed 's/-//g')"
+	shasum=$(sha256sum "$temp/ungoogled-chromium-$pkgver-$pkgrel-x86_64.pkg.tar.zst" | cut -d' ' -f1)
+	sed -i "s/^pkgver=.*/pkgver=$pkgver/" PKGBUILD
+	sed -i "s/^pkgrel=.*/pkgrel=$pkgrel/" PKGBUILD
+	sed -i "s/^sha256sums=.*/sha256sums=(\"$shasum\")/" PKGBUILD
+	git commit . -m "bump version: $pkgver"
+else
+	echo Problem with signature.
+fi
+
