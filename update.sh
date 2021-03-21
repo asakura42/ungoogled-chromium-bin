6ad5f376d05625467ab8fe642a8211b68e5e4c2b
asakura42 <51290160+asakura42@users.noreply.github.com>
2021-03-21 08:34:10 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,13 +4,13 @@ links=$(echo "$zst" | sed 's|^|https://repo.vin.ovh/arch/x86_64/|')
 curl -LO "$links.sig"
 curl -LO "$links"
 gpg --recv-keys 3DEA62513C8035383A245A12E5786B42E8E5D565
-gpg "$temp/$zst.sig"
+gpg "$zst.sig"
 if [ $? -eq 0 ]
 then
 	version=$(ls -1 | grep "ungoogled-chromium.*zst$" | head -n1)
 	pkgver="$(egrep -o "([0-9]{1,}\.)+[0-9]{1,}" <<< "$version")"
 	pkgrel="$(grep -o "\-[[:digit:]]*\-" <<< "$version" | sed 's/-//g')"
-	shasum=$(sha256sum "$temp/ungoogled-chromium-$pkgver-$pkgrel-x86_64.pkg.tar.zst" | cut -d' ' -f1)
+	shasum=$(sha256sum "ungoogled-chromium-$pkgver-$pkgrel-x86_64.pkg.tar.zst" | cut -d' ' -f1)
 	sed -i "s/^pkgver=.*/pkgver=$pkgver/" PKGBUILD
 	sed -i "s/^pkgrel=.*/pkgrel=$pkgrel/" PKGBUILD
 	sed -i "s/^sha256sums=.*/sha256sums=(\"$shasum\")/" PKGBUILD
