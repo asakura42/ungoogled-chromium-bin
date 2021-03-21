98eee7b261892007bf73fbf16a461646890025d9
asakura42 <51290160+asakura42@users.noreply.github.com>
2021-03-21 08:33:46 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-temp=$(mktemp -d)
 zst=$(curl -s "https://repo.vin.ovh/arch/x86_64/" | grep ungoogled | grep -o "ungoogled.*zst" | sort -u)
 links=$(echo "$zst" | sed 's|^|https://repo.vin.ovh/arch/x86_64/|')
-curl -L -O --output-dir "$temp" "$links.sig"
-curl -L -O --output-dir "$temp" "$links"
+curl -LO "$links.sig"
+curl -LO "$links"
 gpg --recv-keys 3DEA62513C8035383A245A12E5786B42E8E5D565
 gpg "$temp/$zst.sig"
 if [ $? -eq 0 ]
 then
-	version=$(ls -1 "$temp" | head -n1)
+	version=$(ls -1 | grep "ungoogled-chromium.*zst$" | head -n1)
 	pkgver="$(egrep -o "([0-9]{1,}\.)+[0-9]{1,}" <<< "$version")"
 	pkgrel="$(grep -o "\-[[:digit:]]*\-" <<< "$version" | sed 's/-//g')"
 	shasum=$(sha256sum "$temp/ungoogled-chromium-$pkgver-$pkgrel-x86_64.pkg.tar.zst" | cut -d' ' -f1)
@@ -19,4 +18,3 @@ then
 else
 	echo Problem with signature.
 fi
-
